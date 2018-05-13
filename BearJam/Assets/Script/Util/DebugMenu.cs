f621bf8544b05c0a89f8e7b0200cb2dbc6a4d249
Galen Manuel <galen.manuel@gmail.com>
2018-05-13 12:49:32 -0400

    Asset Update + More World Movement
    - Update world assets
    - Added transition to time of day to world

---
+++ BearJam/Assets/Script/Util/DebugMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugMenu : MonoBehaviour {
+	/* ENUMS */
+
+
+	/* CONSTANTS */
+
+
+	/* PUBLIC VARS */
+
+
+	/* PRIVATE VARS */
+
+	private CanvasGroup _canvasGroup;
+
+	/* INITIALIZATION */
+
+	private void Awake() {
+		_canvasGroup = GetComponent<CanvasGroup>();
+	}
+
+	/* PROPERTIES */
+
+
+	/* PUBLIC FUNCTIONS */
+
+
+	/* PRIVATE FUNCTIONS */
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.D)) {
+			_canvasGroup.interactable = !_canvasGroup.interactable;
+			_canvasGroup.alpha = _canvasGroup.interactable ? 1.0f : 0.0f;
+		}
+	}
+
+	/* EVENT HANDLERS */
+
+}
--- BearJam/Assets/Script/World.cs
+++ BearJam/Assets/Script/World.cs
@@ -2,16 +2,24 @@
 using DG.Tweening;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class World : MonoBehaviour {
 	/* ENUMS */
 
+	public enum TimeOfDay {
+		Day = 0,
+		Dawn = 95,
+		Night = 180,
+		Dusk = 270,
+	}
 
 	/* CONSTANTS */
 
 
 	/* PUBLIC VARS */
 
+	public bool completelyHideWater;
 
 	/* PRIVATE VARS */
 
@@ -19,30 +27,41 @@ public class World : MonoBehaviour {
 
 	[Header("Linkages")]
 	[SerializeField] private RectTransform _skyLayerRT;
+	[SerializeField] private RectTransform _celestialBodiesLayerRT;
 	[SerializeField] private RectTransform _cloudLayerRT;
+	[SerializeField] private RectTransform _buildingLayerRT;
+	[SerializeField] private RectTransform _treeLayerRT;
 	[SerializeField] private RectTransform _landLayerRT;
 	[SerializeField] private RectTransform _wetSandLayerRT;
 	[SerializeField] private Image _wetSandImage;
 	[SerializeField] private RectTransform _waterLayerRT;
 
 	private Sequence _waterSequence;
+	private Tween _cloudTween;
+	private TimeOfDay _currentTimeOfDay;
 
 	/* INITIALIZATION */
 
 	private void Start() {
+		_waterLayerRT.localScale = completelyHideWater ? new Vector3(0.8f, 0.8f, 0.8f) : new Vector3(0.93f, 0.93f, 0.93f);
+		_waterLayerRT.DORotate(new Vector3(0, 0, -360), _dayLength * 3, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
+
 		//_skyLayerRT.DORotate(new Vector3(0, 0, 360), _dayLength, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
-		_cloudLayerRT.DORotate(new Vector3(0, 0, 360), _dayLength, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
+		_cloudTween = _cloudLayerRT.DORotate(new Vector3(0, 0, 360), _dayLength, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
 
 		_waterSequence = DOTween.Sequence();
+		_waterSequence.InsertCallback(0.0f, () => _wetSandImage.SetAlpha(0.0f));
 		// Scale up the water FAST - SLOW easing
-		_waterSequence.Insert(0.0f, _waterLayerRT.DOScale(1.05f, 1.0f).SetEase(Ease.OutCirc));
+		_waterSequence.Insert(1.0f, _waterLayerRT.DOScale(1.05f, 2.0f).SetEase(Ease.OutCirc)); // ends at 2.0f
 		// At end set alpha of wet sand to full
-		_waterSequence.InsertCallback(1.0f, () => _wetSandImage.SetAlpha(1.0f));
+		_waterSequence.InsertCallback(3.0f, () => {
+			_wetSandLayerRT.rotation = _waterLayerRT.rotation;
+			_wetSandImage.SetAlpha(1.0f);
+		});
 		// Scale down the water SLOW - FAST
-		_waterSequence.Insert(1.0f, _waterLayerRT.DOScale(0.95f, 1.0f).SetEase(Ease.InCirc));
+		_waterSequence.Insert(3.0f, _waterLayerRT.DOScale(completelyHideWater ? 0.80f : 0.93f, 1.5f).SetEase(Ease.InOutCirc)); // ends at 3.0f
 		// Fade out wet sand
-		_waterSequence.Insert(1.0f, _wetSandImage.DOFade(0.0f, 2.0f).SetEase(Ease.Linear));
-		_waterSequence.AppendInterval(1.0f);
+		_waterSequence.Insert(3.0f, _wetSandImage.DOFade(0.0f, 2.0f).SetEase(Ease.Linear)); // ends at 4.0f
 
 		_waterSequence.SetLoops(-1);
 	}
@@ -52,10 +71,32 @@ public class World : MonoBehaviour {
 
 	/* PUBLIC FUNCTIONS */
 
+	public void DEBBUG_GoToTimeOfDay(int pTime) {
+		GoToTimeOfDay((TimeOfDay)pTime);
+	}
+
+	public void GoToTimeOfDay(TimeOfDay pTimeOfDay) {
+		if(_currentTimeOfDay != pTimeOfDay) {
+			float offset = (int)_currentTimeOfDay - (int)pTimeOfDay;
+			_currentTimeOfDay = pTimeOfDay;
+			// start spinning sequence
+			_cloudTween.timeScale = 5;
+			_waterSequence.timeScale = 10;
+			_skyLayerRT.DORotate(new Vector3(0, 0, (360 * 5) + offset), 2.0f, RotateMode.LocalAxisAdd);
+			//_celestialBodiesLayerRT.DORotate(new Vector3(0, 0, (360 * 5) + offset), 2.0f, RotateMode.LocalAxisAdd);
+			_buildingLayerRT.DORotate(new Vector3(0, 0, (360 * 5) + offset), 2.0f, RotateMode.LocalAxisAdd);
+			_treeLayerRT.DORotate(new Vector3(0, 0, (360 * 5) + offset), 2.0f, RotateMode.LocalAxisAdd);
+			_landLayerRT.DORotate(new Vector3(0, 0, (360 * 5) + offset), 2.0f, RotateMode.LocalAxisAdd).OnComplete(OnTimeOfDaySettled);
+		}
+	}
 
 	/* PRIVATE FUNCTIONS */
 
 
 	/* EVENT HANDLERS */
 
+	private void OnTimeOfDaySettled() {
+		_cloudTween.timeScale = 1;
+		_waterSequence.timeScale = 1;
+	}
 }
